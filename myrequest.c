4bb2eb1337a10baa3aeb1633645e20dfbd1a1065
Dan Borah <dborah@middlebury.edu>
2022-02-09 20:10:02 -0500

    removed printfs used previously for debugging

--- myrequest.c
+++ myrequest.c
@@ -173,14 +173,11 @@ parse_body(char *body, int body_len) {
         position += strlen(curr_pair->value) + 1;
     }
 
-    printf("value:%s\n", curr_pair->value);
-
     // Adding better null terminator
     char *last_val = curr_pair->value;
 
     while (isalpha(*last_val) || isdigit(*last_val)) {
         last_val++;
-        printf("next\n");
     }
 
     *last_val = '\0';
